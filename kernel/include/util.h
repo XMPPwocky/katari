a6d00590676504d872f53b755b4f26efcb944970
wocky <wocky@xmppwocky.net>
2012-04-06 19:08:57 +0000

    add a few more handy types to util.h

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -2,7 +2,17 @@
 
 #define NULL ((void *)0)
 
+typedef unsigned long size_t;
+typedef unsigned long ptrdiff_t;
+
+
 #if WORD_SIZE == 4
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed int int32_t;
+typedef unsigned int uint32_t;
+typedef signed long long int64_t;
+typedef unsigned long long uint64_t;
 #endif
