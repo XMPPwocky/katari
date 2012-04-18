b4e97f1810ac04bacd8ba68708d26d48b37e35a6
wocky <wocky@xmppwocky.net>
2012-04-18 22:37:04 +0000

    Write headers

--- include/kernel/thread.h
+++ include/kernel/thread.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include	"kernel/kernel.h"
+
+typedef int tid_t;
+
+#define MAX_THREADS 65536
+
 struct ThreadState {
 	int retstate[2]; /* LR and CPSR */
 	int registers[15]; /* r0-r14 */
 };
+
+struct Thread {
+	struct ThreadState *state;
+	
+	tid_t id;
+};
+
+extern struct Thread *thread_table[MAX_THREADS];
---
+++ src/kernel/thread.c
@@ -0,0 +1,4 @@
+#include	"kernel/kernel.h"
+#include	"kernel/thread.h"
+
+struct Thread *thread_table[MAX_THREADS];
